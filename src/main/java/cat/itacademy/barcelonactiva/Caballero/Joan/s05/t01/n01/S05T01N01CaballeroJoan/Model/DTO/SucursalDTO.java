72906068b53c1337ef804269cce119ceecd6795f
JoanCaballero <caballero.portoles.joan@gmail.com>
2024-02-22 11:52:40 +0100

    Doncs no se que he canviat

--- src/main/java/cat/itacademy/barcelonactiva/Caballero/Joan/s05/t01/n01/S05T01N01CaballeroJoan/Model/DTO/SucursalDTO.java
+++ src/main/java/cat/itacademy/barcelonactiva/Caballero/Joan/s05/t01/n01/S05T01N01CaballeroJoan/Model/DTO/SucursalDTO.java
@@ -21,9 +21,6 @@ public class SucursalDTO {
         this.tipusSucursal = tipusSucursal;
     }
 
-    //private static final List<String> paisos = Arrays.asList("Àustria", "Bèlgica", "Bulgària", "Croàcia", "República de Xipre", "República Txeca", "Dinamarca", "Estònia", "Finlàndia", "França", "Alemanya", "Grècia", "Hongria", "Irlanda", "Itàlia", "Letònia", "Lituània", "Luxemburg", "Malta", "Països Baixos", "Polònia", "Portugal", "Romania", "Eslovàquia", "Eslovènia", "Espanya", "Suècia");
-
-
     public Integer getPk_SucursalID() {
         return pk_SucursalID;
     }
@@ -56,29 +53,4 @@ public class SucursalDTO {
         this.tipusSucursal = tipusSucursal;
     }
 
-//    public static SucursalDTO toSucursalDTO(Sucursal sucursal) {
-//        SucursalDTO sucursalDTO = new SucursalDTO();
-//
-//        sucursalDTO.setPk_SucursalID(sucursal.getPk_SucursalID());
-//        sucursalDTO.setNomSucursal(sucursal.getNomSucursal());
-//        sucursalDTO.setPaisSucursal(sucursal.getPaisSucursal());
-//
-//        if (paisos.contains(sucursal.getPaisSucursal())){
-//            sucursalDTO.setTipusSucursal("UE");
-//        } else {
-//            sucursalDTO.setTipusSucursal("Fora UE");
-//        }
-//
-//        return sucursalDTO;
-//    }
-//
-//    public static Sucursal fromSucursalDTO(SucursalDTO sucursalDTO){
-//        Sucursal sucursal = new Sucursal();
-//
-//        sucursal.setPk_SucursalID(sucursalDTO.getPk_SucursalID());
-//        sucursal.setNomSucursal(sucursalDTO.getNomSucursal());
-//        sucursal.setPaisSucursal(sucursalDTO.getPaisSucursal());
-//
-//        return sucursal;
-//    }
 }
